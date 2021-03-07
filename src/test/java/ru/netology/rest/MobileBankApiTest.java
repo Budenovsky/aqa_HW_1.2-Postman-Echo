31b8783d63122295e78aefaec547a69df9346839
Boris.Budenovsky <fpqb00@gmail.com>
2021-03-07 13:13:50 +0300

    initial commit

---
+++ src/test/java/ru/netology/rest/MobileBankApiTest.java
@@ -0,0 +1,26 @@
+package ru.netology.rest;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class MobileBankApiTest {
+    @Test
+    void shouldTestPostRequest() {
+// Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("hey sweety") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", Matchers.equalTo("hey honey"))
+        ;
+    }
+}
